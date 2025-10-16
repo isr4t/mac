1614060bd33810dfa2e359fe7499330dd77455c9
Israt Jahan <isratjahan@Israts-MacBook-Pro.local>
2025-10-16 10:44:17 +0600

    initial commit

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,27 +1,62 @@
 import React from "react";
+import clsx from "clsx";
+import { Box, OrbitControls } from "@react-three/drei";
+
+import { Canvas } from "@react-three/fiber";
+
+import useMacbookStore from "../store";
 
 const ProductViewer = () => {
+
+    const { color, scale , setColor , setScale } = useMacbookStore();
+
   return (
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
       <div className="controls">
-        <p className="info">MacbookPro 16" in Space Black </p>
+        <p className="info">MacbookPro {scale} in {color} </p>
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
-            <div className="bg-neutral-300" />
-            <div className="bg-neutral-900" />
+
+
+            <div 
+            onClick={() => setColor('#adb5bd')}
+             className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
+
+             />
+           <div 
+            onClick={() => setColor('#2e2c2e')}
+             className={clsx('bg-neutral-900', color === '#adb5bd' && 'active')}
+
+             />
+
+
+
           </div>
           <div className="size-control">
-            <div>
-              <p>14"</p>
+              <div 
+            onClick={() => setScale(0.06)}
+             className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
+
+             >
+                <p>14"</p>
             </div>
-            <div>
-              <p>16"</p>
+                <div 
+            onClick={() => setScale(0.08)}
+             className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
+
+             >
+                <p>16"</p>
             </div>
           </div>
         </div>
       </div>
-      <p className="text-white text-4xl ">Render Canvas</p>
+    <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1 , far: 100}}>
+        <Box position={[0, 0, 0]} scale={10 * scale } material-color={color}/>
+
+        <OrbitControls enableZoom={false}/>
+
+    </Canvas>
     </section>
   );
 };
